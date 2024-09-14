88a8a8c009abfcd4d7d9d23518b9518491539a54
virginianjoki <njokivirginia0619@gmail.com>
2024-09-14 20:21:48 +0300

    if,elif,else

---
+++ functions.py
@@ -0,0 +1,9 @@
+njoki_age = 19
+age_at_college = 17
+
+if njoki_age < age_at_college:
+    print("enjoy highschool babes")
+elif njoki_age == age_at_college:
+    print("enjoy college babes")  
+else:
+    print("hope you figure out your life babes")      
