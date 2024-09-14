92771e77bb5f7fc49b0c8506cddccba134dd8fbd
virginianjoki <njokivirginia0619@gmail.com>
2024-09-14 20:42:39 +0300

    def_functions

---
+++ def_functions.py
@@ -0,0 +1,7 @@
+def print_njoki():
+    text = "njoki ni mnoma"
+    print(text)
+
+print_njoki()
+#def means define
+#you must type 'print_njoki()' before you run code in order to call the 'def_function'
